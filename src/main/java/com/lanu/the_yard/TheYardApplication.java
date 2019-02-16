d06c81a31f0f2d0d53aa677f60b6a7f19d12394e
Serhiy K <mrlanu@gmail.com>
2019-02-15 19:19:45 -0600

    init commit

---
+++ src/main/java/com/lanu/the_yard/TheYardApplication.java
@@ -0,0 +1,14 @@
+package com.lanu.the_yard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheYardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheYardApplication.class, args);
+    }
+
+}
+
